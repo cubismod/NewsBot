13cf3b4fe9da6d0dcfa2493f14d5b0c806910606
Ryan Wallace <ryanwallace024@gmail.com>
2020-11-25 16:53:06 -0600

    functioning bot

---
+++ app.js
@@ -0,0 +1,62 @@
+// Copyright 2020 cubis
+var axios = require('axios').default;
+require('dotenv').config();
+
+async function newsRequest() {
+    var options = {
+        method: 'GET',
+        url: 'https://newscatcher.p.rapidapi.com/v1/latest_headlines',
+        params: {topic: 'politics', lang: 'en', country: 'us', media: 'True'},
+        headers: {
+            'x-rapidapi-key': process.env.NEWSTOK,
+            'x-rapidapi-host': 'newscatcher.p.rapidapi.com'
+        }
+    };
+    try {
+        const response = await axios.request(options);
+        console.log(response.data);
+        return response.data;
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+function discordPush(content) {
+    var articles = content.articles
+    for(var index in articles) {
+        var story = articles[index];
+        // randomize the color of each embed
+        var options = {
+            method: 'POST',
+            url: process.env.WEBHOOK,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            data :{
+                "embeds": [
+                    {
+                    "title": story.title,
+                    "description": story.summary,
+                    "url": story.link,
+                    "color": Math.floor(Math.random()*16777215),
+                    "image": {
+                        url: story.media,
+                    },
+                    "author": {
+                        "name": story.clean_url,
+                    }
+                }]
+            }
+        }
+        axios(options);
+    }
+}
+
+function refreshNews() {
+    newsRequest().then(discordPush);
+}
+
+console.log('NewsBot is up and running');
+refreshNews();
+// refresh news every hour
+setTimeout(refreshNews, "3.6e+6")
